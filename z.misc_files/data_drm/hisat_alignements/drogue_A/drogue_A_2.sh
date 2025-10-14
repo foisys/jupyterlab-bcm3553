a013f2051569f2d48e78293af381fc1798b38dc8
Sylvain Foisy <sylvain.foisy@diploide.net>
2025-10-14 11:33:36 -0400

    Fichiers de démo pour le script YAML+HISAT

---
+++ z.misc_files/data_drm/hisat_alignements/drogue_A/drogue_A_2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+#SBATCH --account=compteUsager 
+#SBATCH --job-name=hisat2_run 
+#SBATCH --mail-user=mon.adresse@mon.serveur 
+#SBATCH --mail-type=ALL 
+#SBATCH --time=4:0:0 
+#SBATCH --nodes=1 
+#SBATCH --cpus-per-task=4 
+#SBATCH --mem=8G 
+#SBATCH --output=/Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/hisat_alignements/control_neg/control_neg_1.log
+#SBATCH --output=/Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/hisat_alignements/control_neg/control_neg_2.log
+#SBATCH --output=/Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/hisat_alignements/control_pos/control_pos_1.log
+#SBATCH --output=/Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/hisat_alignements/control_pos/control_pos_2.log
+#SBATCH --output=/Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/hisat_alignements/drogue_A/drogue_A_1.log
+#SBATCH --output=/Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/hisat_alignements/drogue_A/drogue_A_2.log
+hisat2 -x /ou/se/trouve/hisat2/index \
+--phred-64 \
+--rna-strandness RF \
+--downstream-transcriptome-assembly \
+--reorder \
+--threads 4 \
+-1 /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/donnees_brutes/control_neg/control_neg_1.R1.fastqdemo \
+-2 /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/donnees_brutes/control_neg/control_neg_1.R2.fastqdemo \
+-S /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/hisat_alignements/control_neg/control_neg_1.sam-1 /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/donnees_brutes/control_neg/control_neg_2.R1.fastqdemo \
+-2 /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/donnees_brutes/control_neg/control_neg_2.R2.fastqdemo \
+-S /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/hisat_alignements/control_neg/control_neg_2.sam-1 /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/donnees_brutes/control_pos/control_pos_1.R1.fastqdemo \
+-2 /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/donnees_brutes/control_pos/control_pos_1.R2.fastqdemo \
+-S /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/hisat_alignements/control_pos/control_pos_1.sam-1 /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/donnees_brutes/control_pos/control_pos_2.R1.fastqdemo \
+-2 /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/donnees_brutes/control_pos/control_pos_2.R2.fastqdemo \
+-S /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/hisat_alignements/control_pos/control_pos_2.sam-1 /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/donnees_brutes/drogue_A/drogue_A_1.R1.fastqdemo \
+-2 /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/donnees_brutes/drogue_A/drogue_A_1.R2.fastqdemo \
+-S /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/hisat_alignements/drogue_A/drogue_A_1.sam-1 /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/donnees_brutes/drogue_A/drogue_A_2.R1.fastqdemo \
+-2 /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/donnees_brutes/drogue_A/drogue_A_2.R2.fastqdemo \
+-S /Users/foisys/Documents/Cours_UdeM_CVM-Contenu/UdeM/jupyterlab-bcm3553/z.misc_files/data_drm/hisat_alignements/drogue_A/drogue_A_2.sam
